b87a4d360484b90cda386c6303b298d43f17e3e1
Aryan Khurana <aryankhurana1511@gmail.com>
2022-04-02 13:01:39 -0400

    Added 2 more pattern questions

---
+++ 002-JAVA Basics/Patterns/Pattern07.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/*
+Question 07:
+        1
+      2 1 2
+    3 2 1 2 3
+  4 3 2 1 2 3 4
+5 4 3 2 1 2 3 4 5
+*/
+
+import java.util.Scanner;
+
+public class Pattern07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int row = 1; row <= n; row++) {
+
+            for (int space = 0; space < n - row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ 002-JAVA Basics/Patterns/Pattern08.java
@@ -0,0 +1,39 @@
+package com.company;
+
+/*
+Question 08:
+   1
+  212
+ 32123
+4321234
+ 32123
+  212
+   1
+*/
+
+import java.util.Scanner;
+
+public class Pattern08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int row = 1; row <= 2 * n; row++) {
+
+            int c = row > n ? 2 * n - row: row;
+
+            for (int space = 0; space < n - c; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
